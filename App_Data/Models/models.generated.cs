9393f86c2f8c88bd5756cff7776dd567e2cce685
tanelij <taneli.juga93@gmail.com>
2019-09-26 08:02:59 +0300

    created spell school page

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "994e832904a04f3e")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.15")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
